d707829d23b58326182c3c17fb5f52d275feda6b
Jim Schubert <james.schubert@gmail.com>
2020-02-29 15:43:07 -0500

    Add some placeholder command line args

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"os"
+	"strings"
+)
+
 var version = ""
 var date = ""
 var commit = ""
 var projectName = ""
 
+
+var opts struct {
+	Owner string `short:"o" long:"owner" description:"GitHub Owner/Org name (required)" required:"true"`
+
+	Repo string `short:"r" long:"repo" description:"GitHub Repo name (required)" required:"true"`
+
+	Version bool `short:"v" long:"version" description:"Display version information"`
+}
+
+const parseArgs = flags.HelpFlag | flags.PassDoubleDash
+
 func main() {
-	
+	parser := flags.NewParser(&opts, parseArgs)
+	_, err := parser.Parse()
+	if err != nil {
+		flagError := err.(*flags.Error)
+		if flagError.Type == flags.ErrHelp {
+			parser.WriteHelp(os.Stdout)
+			return
+		}
+
+		if flagError.Type == flags.ErrUnknownFlag {
+			_, _ = fmt.Fprintf(os.Stderr, "%s. Please use --help for available options.\n", strings.Replace(flagError.Message, "unknown", "Unknown", 1))
+			return
+		}
+		_, _ = fmt.Fprintf(os.Stderr, "Error parsing command line options: %s\n", err)
+		return
+	}
+
+	if opts.Version {
+		fmt.Printf("%s %s (%s)\n", projectName, version, commit)
+		return
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gh-changelog
 
 go 1.14
+
+require github.com/jessevdk/go-flags v1.4.0
